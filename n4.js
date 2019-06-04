c531cf8953a29e2f5b8589c3756d05b7603c28bc
huanshenyi <txy1226052>
2019-06-04 18:29:51 +0900

    会話ルーム制作ちゅ,画面共有の四角は完成

---
+++ n4.js
@@ -0,0 +1,13 @@
+var http = require("http");
+var io = require("socket.io")
+
+var server = http.createServer((req,res)=>{
+   
+})
+server.listen(2183);
+
+io.listen(server).on("connection",(user)=>{
+   user.on('divNode',(json)=>{
+      user.broadcast.emit('style',json)
+   })
+});
