b6a27e628179555744b884485b5f1a3b186a9ea2
huanshenyi <txy1226052>
2019-06-04 11:31:39 +0900

    基本完成

--- n.js
+++ n.js
@@ -5,7 +5,10 @@ var server = http.createServer((req,res)=>{
    
 })
 server.listen(2183);
+const arr = [];
 io.listen(server).on("connection",(user)=>{
+  user.broadcast.emit('msg','誰か入ってきた')
+  arr.push(user);
   //console.log("誰かきた!")
 //   setInterval(()=>{
 //     user.emit("aaa",Math.random());
@@ -14,7 +17,20 @@ io.listen(server).on("connection",(user)=>{
 //     console.log(str)
 // })
     user.on('msg',(str)=>{
-        console.log(str);
-        user.emit('msg',str);
+        // console.log(str);
+        // user.emit('msg',str);
+        
+        //全部userにメッセージ
+        // arr.forEach((user)=>{
+        //   user.emit('msg',str)
+        // })
+
+        // またオンラインして、自分以外のuserにメッセージ
+        user.broadcast.emit('msg',str);
+        user.emit('msg','自分:'+str);
+    });
+    user.on('disconnect',()=>{
+      user.broadcast.emit('msg','誰かログアウトした')
     })
+
 })
