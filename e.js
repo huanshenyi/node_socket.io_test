da167641bfbfe32a0551385617f766bc3706b185
huanshenyi <txy1226052>
2019-06-07 15:26:52 +0900

    mini_game_over

--- e.js
+++ e.js
@@ -2,6 +2,7 @@ const express = require("express");
 const io = require("socket.io");
 var server = express();
 var http = require("http").Server(server);
+var blood = 1000,max = 1000;
 
 server.use('/lll',(req,res)=>{
    //console.log(1);
@@ -12,6 +13,13 @@ http.listen(2131);
 server.use("/",express.static('./'))
 
 io.listen(http).on('connection',(user)=>{
-   console.log(1); 
-   user.emit('msg',1);
+//    console.log(1); 
+//    user.emit('msg',1);
+   user.emit('blood',blood/max);
+   user.on('kill',(str)=>{
+     var ran = Math.random()*5+5;
+     blood -= ran;
+     user.emit('kill',[blood/max,ran]);
+     user.broadcast.emit('kill',[blood/max,ran]);
+   });
 });
